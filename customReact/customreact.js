64cad1607816aa6a7af154a5bd4c645b80e94d35
Rohit Senapati <rohitsenapati08@gmail.com>
2024-11-20 22:37:38 +0530

    Custom React

---
+++ customReact/customreact.js
@@ -0,0 +1,19 @@
+function customRender(reactElement,container){
+  const domElement=document.createElement(reactElement.type);
+  domElement.innerHTML=reactElement.children;
+  domElement.setAtrribute('href',reactElement.props.href);
+  domElement.setAtrribute('target',reactElement.props.target);
+  container.appendChild(domElement);
+}
+
+const reactElement={
+  type:'a',
+  props:{
+    href:'https://google.com',
+    target:'_blank'
+  },
+  children:'click me to visit google'
+}
+const mainContainer=document.querySelector('#root');
+
+customRender(reactElement,mainContainer)
